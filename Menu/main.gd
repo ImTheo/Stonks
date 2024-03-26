25ed7e74cd70a464a4243171dcfff4aa27da2c0d
theoSisa <153256596+theoSisa@users.noreply.github.com>
2024-03-26 18:33:07 -0500

    first commit

---
+++ Menu/main.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	print("hola mundo")
+
+func _on_button_buy_items_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_button_view_invoices_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_exit_pressed():
+	pass # Replace with function body.
